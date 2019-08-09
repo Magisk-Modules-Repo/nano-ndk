e19f6d88d22e6105a2e13b5b81677a6f30b65b7e
osm0sis <osm0sis@outlook.com>
2019-08-09 18:10:20 -0300

    support SAR Magisk

--- install.sh
+++ install.sh
@@ -77,8 +77,7 @@ else
     etc=$mnt$magisk/etc;
     bin=$mnt$magisk/bin;
   else
-    mount -o rw,remount /system;
-    mount /system;
+    mount -o rw,remount /system || mount /system || mount -o rw,remount / && sar=1;
     etc=$root/system/etc;
     bin=$root/system/xbin;
   fi;
@@ -126,6 +125,7 @@ ui_print " ";
 ui_print "Unmounting...";
 test "$suimg" && umount $mnt;
 test "$loop" && losetup -d $loop;
+test "$sar" && mount -o ro,remount /;
 umount /system;
 umount /data;
 umount /cache;
